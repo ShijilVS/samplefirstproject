ed77b0a95cd77e2bfbd1586d6757c314e10fe233
ShijilVS <shijilvassem000@gmail.com>
2022-11-21 15:16:03 +0530

    commit

---
+++ bin/test.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+ String name = "me";
+ int age = 20;
+ int phone = 9809772131;
+ double mark = 9.9;
+ String email = "shijil@gmail.com";
+ print("---My Resume---");
+ print("My name is $name");
+
+
+}
